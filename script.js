6964870f98f3f4ff4e9394c17bb12313435f700f
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-02-13 14:58:29 +0530

    error solved

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ container.className = "container";
 var Row = document.createElement("div");
 Row.className = "row";
 var Col = document.createElement("div");
-Col.className = "col-4";
+Col.className = "col-lg-12 col-xl-4";
 Col.id = "col1";
 var Col2 = document.createElement("div");
-Col2.className = "col-8";
+Col2.className = "col-lg-12 col-xl-8";
 Col2.id = "col2";
 
 Row.append(Col, Col2);
@@ -15,6 +15,7 @@ document.body.append(container);
 
 var form1 = document.getElementById("col1");
 form1.innerHTML = `
+ 
 <div class=form type=submit >
 <h3 style=color:blue> Form Submission </h3>
 <label> First Name </label><br>
@@ -82,7 +83,7 @@ form1.innerHTML = `
 
 var Tab = document.getElementById("col2");
 Tab.innerHTML = `
-
+ 
 <h3 style=color:blue> Form Table </h3>
 
 <table class=table table-dark>
@@ -160,70 +161,66 @@ Country
 <br>
 <br>
 <div class = container style=text-align:center>
-<button type="button" onclick="clear()" class="btn btn-danger" > Clear Data </button>
-</div>
+<button type="button" onclick="clearData()" class="btn btn-danger" > Clear Data </button>
+</div> 
 `;
 
 
-function data(){
-var tD1 = document.getElementById("td1");
-var fnameval = document.getElementById("fname").value;
-tD1.innerHTML = fnameval;
-var fnameval = document.getElementById("fname").value="";
+function data() {
+  var tableBody = document.querySelector("#col2 tbody");
 
-var tD2 = document.getElementById("td2");
-var lnameval = document.getElementById("lname").value;
-tD2.innerHTML = lnameval;
-var lnameval = document.getElementById("lname").value="";
+  var newRow = document.createElement("tr");
 
-var tD3 = document.getElementById("td3");
-var ad1val = document.getElementById("ad1").value;
-tD3.innerHTML = ad1val;
-var ad1val = document.getElementById("ad1").value="";
+  var fname = document.getElementById("fname").value;
+  newRow.innerHTML += "<td>" + fname + "</td>";
+  document.getElementById("fname").value = "";
 
-var tD4 = document.getElementById("td4");
-var ad2val = document.getElementById("ad2").value;
-tD4.innerHTML = ad2val;
-var ad2val = document.getElementById("ad2").value="";
+  var lname = document.getElementById("lname").value;
+  newRow.innerHTML += "<td>" + lname + "</td>";
+  document.getElementById("lname").value = "";
 
-var tD5 = document.getElementById("td5");
-var pinval = document.getElementById("pin").value;
-tD5.innerHTML = pinval;
-var pinval = document.getElementById("pin").value="";
+  var ad1 = document.getElementById("ad1").value;
+  newRow.innerHTML += "<td>" + ad1 + "</td>";
+  document.getElementById("ad1").value = "";
 
-var tD6 = document.getElementById("td6");
-var selectedval = document.getElementById("selected").value;
-if( +selectedval === 1 ){
-    var val1 = "male";
-    selectedval = val1;
+  var ad2 = document.getElementById("ad2").value;
+  newRow.innerHTML += "<td>" + ad2 + "</td>";
+  document.getElementById("ad2").value = "";
+
+  var pin = document.getElementById("pin").value;
+  newRow.innerHTML += "<td>" + pin + "</td>";
+  document.getElementById("pin").value = "";
+
+  var selected = document.getElementById("selected").value;
+  if (+selected === 1) {
+      selected = "male";
+  } else if (+selected === 2) {
+      selected = "female";
+  } else {
+      selected = "others";
+  }
+  newRow.innerHTML += "<td>" + selected + "</td>";
+  document.getElementById("selected").value = "";
+
+  var checkboxes = document.querySelectorAll(".form-check-input:checked");
+  var checkedValues = Array.from(checkboxes).map(checkbox => checkbox.value);
+  newRow.innerHTML += "<td>" + checkedValues.join(", ") + "</td>";
+
+  var state = document.getElementById("state").value;
+  newRow.innerHTML += "<td>" + state + "</td>";
+  document.getElementById("state").value = "";
+
+  var country = document.getElementById("country").value;
+  newRow.innerHTML += "<td>" + country + "</td>";
+  document.getElementById("country").value = "";
+
+  tableBody.appendChild(newRow);
 }
-else if( +selectedval === 2 ){
-    var val2 = "female";
-    selectedval = val2;
+
+function clearData() {
+  var tableBody = document.querySelector("#col2 tbody");
+  tableBody.innerHTML = "";
 }
-else {
-    var val3 = "others";
-    selectedval = val3;
 
-}
-tD6.innerHTML = selectedval;
-var selectedval = document.getElementById("selected").value="";
-
-var tD7 = document.getElementById("td7");
-var checkedval = document.querySelector("#flexCheckDefault").value;
-tD7.innerHTML = checkedval;
-checkedval = document.querySelector("#flexCheckDefault").value="";
-
-var tD8 = document.getElementById("td8");
-var stateval = document.getElementById("state").value;
-tD8.innerHTML = stateval;
-stateval = document.getElementById("state").value="";
-
-var tD9 = document.getElementById("td9");
-var countryval = document.getElementById("country").value;
-tD9.innerHTML = countryval;
-var countryval = document.getElementById("country").value="";
-
-}
 
  
